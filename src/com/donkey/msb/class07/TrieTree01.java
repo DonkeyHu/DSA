748613b66c3da08a0954bf755c47cd21c988ecb9
DonkeyHu <328021040@qq.com>
2021-11-10 00:02:55 +0800

    msb trie tree

--- src/com/donkey/msb/class07/TrieTree01.java
+++ src/com/donkey/msb/class07/TrieTree01.java
@@ -1,5 +1,8 @@
 package com.donkey.msb.class07;
 
+import java.util.HashMap;
+import java.util.Set;
+
 /**
  * 前缀树
  */
@@ -86,7 +89,7 @@ public class TrieTree01 {
             Node1 node = this.root;
             for (int i = 0; i < str.length; i++) {
                 int x = str[i] - 'a';
-                if (node.nexts[x] == null){
+                if (node.nexts[x] == null) {
                     return 0;
                 }
                 node = node.nexts[x];
@@ -94,4 +97,140 @@ public class TrieTree01 {
             return node.pass;
         }
     }
+
+
+    public static class Node2 {
+        private int pass;
+        private int end;
+        private HashMap<Integer, Node2> next;
+
+        public Node2() {
+            this.pass = 0;
+            this.end = 0;
+            this.next = new HashMap<>();
+        }
+    }
+
+    public static class Trie2 {
+        private Node2 root;
+
+        public Trie2() {
+            root = new Node2();
+        }
+
+        public void insert(String word) {
+            if (word == null) {
+                return;
+            }
+            Node2 node = root;
+            node.pass++;
+            char[] str = word.toCharArray();
+            for (int i = 0; i < str.length; i++) {
+                int x = (int) str[i];
+                if (!node.next.containsKey(x)) {
+                    node.next.put(x, new Node2());
+                }
+                node = node.next.get(x);
+                node.pass++;
+            }
+            node.end++;
+        }
+
+        public void delete(String word) {
+            if (word == null) {
+                return;
+            }
+            Node2 node = root;
+            node.pass--;
+            char[] str = word.toCharArray();
+            for (int i = 0; i < str.length; i++) {
+                int x = (int) str[i];
+                if (--node.next.get(x).pass == 0) {
+                    node.next.remove(x);
+                    return;
+                }
+                node = node.next.get(x);
+            }
+            node.end--;
+        }
+
+        public int search(String word) {
+            if (word == null) {
+                return 0;
+            }
+            Node2 node = root;
+            char[] str = word.toCharArray();
+            for (int i = 0; i < str.length; i++) {
+                int x = (int) str[i];
+                if (!node.next.containsKey(x)) {
+                    return 0;
+                }
+                node = node.next.get(x);
+            }
+            return node.end;
+        }
+
+        public int prefixNum(String word) {
+            if (word == null) {
+                return 0;
+            }
+            Node2 node = root;
+            char[] str = word.toCharArray();
+            for (int i = 0; i < str.length; i++) {
+                int x = (int) str[i];
+                if (!node.next.containsKey(x)) {
+                    return 0;
+                }
+                node = node.next.get(x);
+            }
+            return node.pass;
+        }
+    }
+
+    public static class Right {
+        private HashMap<String, Integer> box;
+
+        public Right() {
+            this.box = new HashMap<>();
+        }
+
+        public void insert(String word) {
+            if (!box.containsKey(word)) {
+                box.put(word, 1);
+            } else {
+                box.put(word, box.get(word) + 1);
+            }
+        }
+
+        public void delete(String word) {
+            if (box.containsKey(word)) {
+                if (box.get(word) == 1) {
+                    box.remove(word);
+                } else {
+                    box.put(word, box.get(word) - 1);
+                }
+            }
+        }
+
+        public int search(String word) {
+            if (box.containsKey(word)) {
+                return box.get(word);
+            } else {
+                return 0;
+            }
+        }
+
+        public int prefixNum(String word) {
+            int count = 0;
+            for (String str : box.keySet()) {
+                if (str.startsWith(word)) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+
+
 }
