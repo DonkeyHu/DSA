2f6931de27393e992c79b6c2a3c63459ec75c9b4
donkey <328021040@qq.com>
2023-12-17 22:00:00 +0800

    msb dp

--- src/com/donkey/msb/class19/Code03_StickersToSpellWord.java
+++ src/com/donkey/msb/class19/Code03_StickersToSpellWord.java
@@ -1,5 +1,8 @@
 package com.donkey.msb.class19;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给定一个字符串str，给定一个字符串类型的数组arr，出现的字符都是小写英文
  * arr每一个字符串，代表一张贴纸，你可以把单个字符剪开使用，目的是拼出str来
@@ -10,54 +13,156 @@ package com.donkey.msb.class19;
  */
 public class Code03_StickersToSpellWord {
 
-    public static int way1(String s, String[] arr) {
-        return process1(s.toCharArray(), arr);
+    public static int way1(String target, String[] arr) {
+        int ans = process1(target, arr);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
     // 什么时候代表用了一张贴纸了？
-    public static int process1(char[] target, String[] arr) {
-        if (target.length == 0) {
+    // (1) 每递归process一次用掉一次贴纸
+    // (2) 一定是target.length()==0的递归才是有效解的递归
+    // (3) for循环里面(i)与(i+1)之间的关系是或的关系，意味着你要么用了(i)的贴纸数，要么是(i+1)的贴纸数，(i)与(i+1)之间是要比较的，取最小值
+    public static int process1(String target, String[] arr) {
+        if (target.length() == 0) {
             return 0;
         }
 
-        int p = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < arr.length; i++) {
-            char[] res = minis(target, arr[i].toCharArray());
-            if (res.length != target.length) {
-                process1(res, arr);
+            String res = minis(target, arr[i]);
+            if (res.length() != target.length()) {
+                min = Math.min(min, process1(res, arr));
             }
         }
-
-        return p == 0 ? 0 : p + 1;
+        return min + (min == Integer.MAX_VALUE ? 0 : 1);
     }
 
-    public static char[] minis(char[] str, char[] sticker) {
+    public static String minis(String rest, String sticker) {
+        char[] c1 = rest.toCharArray();
+        char[] c2 = sticker.toCharArray();
         int[] tmp = new int[26];
-        for (int i = 0; i < str.length; i++) {
-            int index = str[i] - '0' - 49;
-            tmp[index] += 1;
+        for (char c : c1) {
+            tmp[c - 'a'] += 1;
         }
-        for (int i = 0; i < sticker.length; i++) {
-            int index = sticker[i] - '0' - 49;
-            if (tmp[index] > 0) {
-                tmp[index] -= 1;
-            }
+        for (char c : c2) {
+            tmp[c - 'a'] -= 1;
         }
-        StringBuilder s = new StringBuilder();
+        StringBuilder res = new StringBuilder();
         for (int i = 0; i < tmp.length; i++) {
             while (tmp[i] > 0) {
-                char x = (char) (i + '0' + 49);
-                s.append(x);
+                res.append((char) (i + 'a'));
                 tmp[i] -= 1;
             }
         }
-        return s.toString().toCharArray();
+        return res.toString();
+    }
+
+
+    public static int way2(String target, String[] stickers) {
+        int length = stickers.length;
+        int[][] arr = new int[length][26];
+        for (int i = 0; i < length; i++) {
+            String sticker = stickers[i];
+            char[] chars = sticker.toCharArray();
+            for (char c : chars) {
+                arr[i][c - 'a'] += 1;
+            }
+        }
+        int ans = process2(target, arr);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+
+    public static int process2(String target, int[][] arr) {
+        if (target.length() == 0) {
+            return 0;
+        }
+        char[] cTarget = target.toCharArray();
+        int[] tmp = new int[26];
+        for (char c : cTarget) {
+            tmp[c - 'a'] += 1;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            int[] sticker = arr[i];
+            // 包含target的首字母再去minis
+            if (sticker[cTarget[0] - 'a'] > 0) {
+                StringBuilder res = new StringBuilder();
+                for (int j = 0; j < 26; j++) {
+                    if (tmp[j] > 0) {
+                        int num = tmp[j] - sticker[j];
+                        for (int k = 0; k < num; k++) {
+                            res.append((char) (j + 'a'));
+                        }
+                    }
+                }
+                String rest = res.toString();
+                if (rest.length() != target.length()) {
+                    min = Math.min(min, process2(rest, arr));
+                }
+            }
+        }
+        return min + (min == Integer.MAX_VALUE ? 0 : 1);
+    }
+
+    public static int way3(String target, String[] stickers) {
+        int length = stickers.length;
+        int[][] arr = new int[length][26];
+        for (int i = 0; i < length; i++) {
+            String sticker = stickers[i];
+            char[] chars = sticker.toCharArray();
+            for (char c : chars) {
+                arr[i][c - 'a'] += 1;
+            }
+        }
+        Map<String, Integer> cache = new HashMap<>();
+        int ans = process3(target, arr, cache);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    public static int process3(String target, int[][] arr, Map<String, Integer> cache) {
+        if (cache.containsKey(target)) {
+            return cache.get(target);
+        }
+        if (target.length() == 0) {
+            return 0;
+        }
+        char[] cTarget = target.toCharArray();
+        int[] tmp = new int[26];
+        for (char c : cTarget) {
+            tmp[c - 'a'] += 1;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            int[] sticker = arr[i];
+            // 包含target的首字母再去minis
+            if (sticker[cTarget[0] - 'a'] > 0) {
+                StringBuilder res = new StringBuilder();
+                for (int j = 0; j < 26; j++) {
+                    if (tmp[j] > 0) {
+                        int num = tmp[j] - sticker[j];
+                        for (int k = 0; k < num; k++) {
+                            res.append((char) (j + 'a'));
+                        }
+                    }
+                }
+                String rest = res.toString();
+                if (rest.length() != target.length()) {
+                    min = Math.min(min, process2(rest, arr));
+                }
+            }
+        }
+        int x = min + (min == Integer.MAX_VALUE ? 0 : 1);
+        cache.put(target, x);
+        return x;
     }
 
 
     public static void main(String[] args) {
-        String str = "babac";
+        String str = "babacabcabcabc";
         String[] arr = {"ba", "c", "abcd"};
         System.out.println(way1(str, arr));
+        System.out.println(way2(str, arr));
+        System.out.println(way3(str, arr));
     }
 }
