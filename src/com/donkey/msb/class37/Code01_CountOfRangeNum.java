0e3a72b454827774e133d8eaf1b1d42c853dc540
donkey <328021040@qq.com>
2025-06-04 09:30:18 +0800

    count of range num

---
+++ src/com/donkey/msb/class37/Code01_CountOfRangeNum.java
@@ -0,0 +1,18 @@
+package com.donkey.msb.class37;
+
+/**
+ * 327. 区间和的个数
+ *
+ * 给定一个数组arr，和两个整数a和b（a<=b）
+ * 求arr中有多少个子数组，累加和在[a,b]这个范围上
+ * 返回达标的子数组数量
+ *
+ * 思路：
+ * （1）以i结尾的子数组的累加和有多少个在[a,b]，从左往右模型，从左往右遍历i
+ * （2）
+ */
+public class Code01_CountOfRangeNum {
+
+
+
+}
