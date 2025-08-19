6c97123c8bb4eebabb4f2c3aff0035d0b05b682e
donkey <328021040@qq.com>
2025-08-20 00:27:30 +0800

    leetcode

--- src/com/donkey/leetcode/Q005_LongestPalindrome.java
+++ src/com/donkey/leetcode/Q005_LongestPalindrome.java
@@ -9,45 +9,50 @@ import java.util.List;
 public class Q005_LongestPalindrome {
 
     public static String longestPalindrome(String s) {
-        int len = s.length();
-        char[] chars = s.toCharArray();
-        char[] hashChars = new char[2 * len + 1];
-        int index = 0;
-        int newLen = hashChars.length;
-        for (int i = 0; i < newLen; i++) {
-            hashChars[i] = (i & 1) == 0 ? '#' : chars[index++];
+        if (s == null || s.isEmpty() || s.length() == 1) {
+            return s;
         }
 
-        int max = 0;
-        List<Character> ans = new ArrayList<>();
+        char[] m = manacherStr(s);
+        int len = m.length;
 
-        for (int i = 1; i < newLen; i++) {
-            int l = i;
-            int r = i;
-            int j = 1;
-            while ((l - j) >= 0 && (r + j) < newLen && hashChars[l - j] == hashChars[r + j]) {
-                if (j > max) {
-                    max = j;
-                    ans.clear();
-                    for (int k = l - j; k <= r + j; k++) {
-                        ans.add(hashChars[k]);
+        int max = Integer.MIN_VALUE;
+        String ans = "";
+        for (int i = 0; i < len; i++) {
+            int L = i - 1;
+            int R = i + 1;
+            while (L >= 0 && R < len && m[L] == m[R]) {
+                L--;
+                R++;
+            }
+            int l = L + 1;
+            int r = R - 1;
+            if (l >= 0 && r < len && (r - l + 1) > max) {
+                max = r - l + 1;
+                StringBuilder sb = new StringBuilder();
+                for (int j = l; j <= r; j++) {
+                    if (m[j] != '#') {
+                        sb.append(m[j]);
                     }
                 }
-
-                j++;
+                ans = sb.toString();
             }
         }
+        return ans;
+    }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < ans.size(); i++) {
-            if (ans.get(i) != '#') {
-                sb.append(ans.get(i));
-            }
+    public static char[] manacherStr(String s) {
+        int len = s.length();
+        char[] chars = s.toCharArray();
+        char[] res = new char[2 * len + 1];
+        int index = 0;
+        for (int i = 0; i < 2 * len + 1; i++) {
+            res[i] = (i & 1) == 0 ? '#' : chars[index++];
         }
-        return sb.toString();
+        return res;
     }
 
     public static void main(String[] args) {
-        System.out.println(longestPalindrome("cbbd"));
+        System.out.println(longestPalindrome("cbcd"));
     }
 }
